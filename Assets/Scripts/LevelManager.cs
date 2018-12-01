eaea25594b1e9d04ca51e68ec178e1c5ecd3975d
Adam Coyle <adam.coyle@fhnw.ch>
2018-12-01 10:19:12 +0100

    Initial Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string levelName)
+    {
+        StartCoroutine(StartLevel(levelName));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator StartLevel(string levelName)
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour {
+
+    public GameObject player;
+
+    LevelState selectedLevel;
+
+    private void Start()
+    {
+        if (!selectedLevel)
+        {
+            selectedLevel = transform.GetChild(0).GetComponent<LevelState>();
+            player.transform.position = selectedLevel.transform.position;
+        }
+    }
+
+    void Update () {
+		
+	}
+}
