997fc385933182fdc717fd57748ad4359da56a24
Adam Coyle <acoyle22@qub.ac.uk>
2018-12-02 18:21:10 +0100

    Added a basic Enemy which the Player can bounce off of when they jump on their head.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public BoxCollider2D head;
+    public Rigidbody2D enemyRB;
+
+    public float speed;
+
+    bool isActive;
+
+    private void OnBecameVisible()
+    {
+        isActive = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        isActive = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isActive)
+        {
+            enemyRB.velocity = new Vector2(speed * Time.fixedDeltaTime, enemyRB.velocity.y);
+            KillIfHitOnHead(); 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeDirection();
+    }
+
+    private void KillIfHitOnHead()
+    {
+        bool isHeadHit = head.IsTouchingLayers(LayerMask.GetMask("Player"));
+        if (isHeadHit)
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        speed = -speed;
+        //Vector3 newLocalScale = transform.localScale;
+        //newLocalScale.x = -newLocalScale.x;
+        //transform.localScale = newLocalScale;
+    }
+}
